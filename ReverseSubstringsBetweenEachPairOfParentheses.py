41876582c74070bfe05e6274cd880d6c917d7802
AdaSeteye <99335768+AdaSeteye@users.noreply.github.com>
2023-02-12 10:53:38 +0300

    Create ReverseSubstringsBetweenEachPairOfParentheses.py

---
+++ ReverseSubstringsBetweenEachPairOfParentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        st = []
+        for i in range(len(s)):
+            if (s[i] == '('):
+                st.append(i)
+            elif (s[i] == ')'):
+                temp = s[st[-1]:i + 1]
+                s = s[:st[-1]] + temp[::-1] + \
+                   s[i + 1:]
+                del st[-1]
+ 
+        res = ""
+        for i in range(len(s)):
+            if (s[i] != ')' and s[i] != '('):
+                res += (s[i])
+        return res
