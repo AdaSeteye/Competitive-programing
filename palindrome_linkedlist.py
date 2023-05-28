b07370a5d296fc5d39a126814eeda1c2990af7b6
AdaSeteye <99335768+AdaSeteye@users.noreply.github.com>
2023-05-28 07:59:52 +0300

    Update palindrome_linkedlist.py

--- palindrome_linkedlist.py
+++ palindrome_linkedlist.py
@@ -1,24 +1,11 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        arr1 = []
-        arr2 = []
-        prev = None
-        curr = temp = temp2 = temp3 = head
-        while temp2:
-            arr1.append(temp2.val)
-            temp2 = temp2.next
-
-        while curr:
-            temp = curr.next
-            curr.next = prev
-            prev = curr
-            curr = temp
-        temp3 = prev
-        while temp3:
-            arr2.append(temp3.val)
-            temp3 = temp3.next
-        print("arr1", arr1)
-        print("arr2", arr2)
-        if arr1 == arr2:
+        arr = []
+        while head:
+            arr.append(head.val)
+            head = head.next
+        if arr == arr[::-1]:
             return True
-        return False
+        else:
+            return False
+
