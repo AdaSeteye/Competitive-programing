2f7b173571b6dc33c6c6b963890b3ac9f61d2b34
AdaSeteye <99335768+AdaSeteye@users.noreply.github.com>
2023-07-07 09:44:35 +0300

    Create valid_perfect_square.py

---
+++ valid_perfect_square.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l, r = 1, num
+        while l <= r:
+            mid = l + (r - l)//2
+            if mid * mid == num:
+                return True
+            elif mid * mid > num:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False
